91334ce5a86577bd1b8b08c603627f793fd3853c
Alexis Alejandro Calderon <alejandrodev2019@gmail.com>
2025-11-09 02:01:13 -0300

    Added allure-report

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -4,16 +4,15 @@ import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
-import java.awt.*;
 
 public class ProductsPage extends Form {
 
     public ProductsPage() {
         super(By.id("page_wrapper"), "Products Main Page");
     }
-    private final ILabel logo = getElementFactory().getLabel(By.className("app_logo"), "Logo");
+    private final ILabel logoDashboard = getElementFactory().getLabel(By.className("app_logo"), "Logo Dashboard Header");
 
     public String getTitleLogo() {
-       return logo.getText();
+       return logoDashboard.getText();
     }
 }
